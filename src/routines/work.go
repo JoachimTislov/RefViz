ec3f6aea96dbef33aca1a2743221c261c1095724
JoachimTislov <joachim.tislov@gmail.com>
2025-02-23 05:25:38 +0100

    fix: misspell, made go routines do exactly the same work

--- src/routines/work.go
+++ src/routines/work.go
@@ -16,20 +16,20 @@ func StartWork(workers int, jobs []func() error) error {
 		workers = runtime.NumCPU()
 	}
 
+	wg.Add(workers)
 	for i := 0; i < workers; i++ {
-		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			for i, j := range jobs {
-				if err := j(); err != nil {
+			for job := range jobCh {
+				if err := job(); err != nil {
 					ch <- fmt.Errorf("error running job %d: %v", i, err)
 				}
 			}
 		}()
 	}
 
-	for _, path := range jobs {
-		jobCh <- path
+	for _, j := range jobs {
+		jobCh <- j
 	}
 	close(jobCh)
 
